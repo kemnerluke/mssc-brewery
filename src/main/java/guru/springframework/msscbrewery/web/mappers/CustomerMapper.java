b7dbace4784dfccd00e86bbdb7b6816812a77613
Luke <kemnerluke@gmail.com>
2020-01-18 23:16:59 -0500

    added customer mapper

---
+++ src/main/java/guru/springframework/msscbrewery/web/mappers/CustomerMapper.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbrewery.web.mappers;
+
+import guru.springframework.msscbrewery.domain.Customer;
+import guru.springframework.msscbrewery.web.model.CustomerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface CustomerMapper {
+
+    Customer customerDtoToCustomer(CustomerDTO dto);
+
+    CustomerDTO customerToCustomerDto(Customer customer);
+}
